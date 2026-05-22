1d5318185fc535c3c53689c9e1aec01f8af6df0a
Edison Nunes <Edisonunb@gmail.com>
2026-05-22 20:33:57 +0000

    Fiz o Updat da imagem porem aparece isso

--- next.config.ts
+++ next.config.ts
@@ -27,6 +27,18 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
   experimental: {
